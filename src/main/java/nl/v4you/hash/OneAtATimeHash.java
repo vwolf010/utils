0934ce9257c5c811887978790c2515ce8f6be080
vwolf010 <victor_vd_wolf@yahoo.com>
2019-03-13 14:48:47 +0100

    Added OneAtATimeHash

---
+++ src/main/java/nl/v4you/hash/OneAtATimeHash.java
@@ -0,0 +1,47 @@
+package nl.v4you.hash;
+
+import java.util.Arrays;
+
+public class OneAtATimeHash {
+    byte b[];
+    int hash; // defaults to 0
+
+    OneAtATimeHash(byte b[]) {
+        this.b = b;
+    }
+
+    OneAtATimeHash set(byte b[]) {
+        this.b = b;
+        hash=0;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        return Arrays.equals(b, ((OneAtATimeHash) o).b);
+    }
+
+    @Override
+    public int hashCode() {
+        if (hash==0 && b.length>0) {
+            hash = calcHash(b);
+        }
+        return hash;
+    }
+
+    static int calcHash(byte key[]) {
+        int hash = 0;
+
+        for (byte b : key) {
+            hash += (b & 0xff);
+            hash += (hash << 10);
+            hash ^= (hash >>> 6);
+        }
+
+        hash += (hash << 3);
+        hash ^= (hash >>> 11);
+        hash += (hash << 15);
+        return hash;
+    }
+}
